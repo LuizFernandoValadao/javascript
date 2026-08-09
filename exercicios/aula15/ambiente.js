c0333ab97da758a0114b823ca6e2f4a0ca83cecf
LuizFernandoValadao <l17996021823@gmail.com>
2026-08-09 15:06:49 -0300

    aula15

---
+++ exercicios/aula15/ambiente.js
@@ -0,0 +1,12 @@
+let num = [5, 8, 2, 9, 3]
+console.log('------------------------------------------------------')
+console.log(`Nosso vetor é o ${num} e tem ${num.length} elementos`)
+console.log(`O primeiro valor do vetor é ${num[0]}`)
+console.log(`O ultimo valor do vetor é ${num[num.length-1]}`)
+num.sort()
+console.log(`O vetor em ordem fica: ${num}`)
+console.log('------------------------------------------------------')
+
+for (let c = 0; c <= 4; c++) {
+    console.log(num[c])
+}
