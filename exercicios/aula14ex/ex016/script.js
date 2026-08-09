59c4b4d820f51363c2284df23cd5fefd1ba8df1b
LuizFernandoValadao <l17996021823@gmail.com>
2026-08-09 13:42:54 -0300

    ex016

--- exercicios/aula14ex/ex016/script.js
+++ exercicios/aula14ex/ex016/script.js
@@ -1,24 +1,37 @@
-var txti = document.getElementById('txtinc')
-var txtf= document.getElementById('txtfim')
-var txtp = document.getElementById('txtpas')
-
 function contar() {
-    if (txti == null || txtf == null || txtp == null) {
-        alert('Preencha os campos!')
+    var txti = document.getElementById('txtinc')
+    var txtf= document.getElementById('txtfim')
+    var txtp = document.getElementById('txtpas')
+    if (txti.value.length == 0 || txtf.value.length == 0 || txtp.value.length == 0)  {
+        alert('[ERRO] Faltam dados!')
     } else {
         var inicio = Number(txti.value)
         var fim = Number(txtf.value)
         var passo = Number(txtp.value)
-        if (passo == 0) {
+        if (fim < inicio) {
+            if (passo == 0) {
             passo = 1
         }
-        var res = document.getElementById('res')
-        res.innerHTML = "Contando <br/>"
-        for (var c = inicio; c <= fim; c += passo) {
-            res.innerHTML += `${c}&#x1F449`
-        res.innerHTML += '&#x1F3F4'
+            var res = document.getElementById('res')
+            res.innerHTML = "Contando <br/>"
+            for (var c = inicio; c >= fim; c -= passo) {
+                res.innerHTML += `${c} \u{1F449}`
+        
+            }
+            res.innerHTML += '\u{1F3F4}'
+        } else {
+           if (passo == 0) {
+            passo = 1
+            }
+            var res = document.getElementById('res')
+            res.innerHTML = "Contando <br/>"
+            for (var c = inicio; c <= fim; c += passo) {
+                res.innerHTML += `${c} \u{1F449}`
+        
+            }
+            res.innerHTML += '\u{1F3F4}' 
         }
-    
+        
     }
     
 }
