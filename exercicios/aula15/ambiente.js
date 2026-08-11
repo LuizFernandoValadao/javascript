d68c06b2393c01577c41a2ea33d4e757ee1c72e6
LuizFernandoValadao <l17996021823@gmail.com>
2026-08-10 21:51:57 -0300

    Use for...in loop to iterate array positions
    Replace traditional for loop with for...in loop to demonstrate iterating over array positions. The new approach explicitly shows the index position alongside each value.

--- exercicios/aula15/ambiente.js
+++ exercicios/aula15/ambiente.js
@@ -6,7 +6,11 @@ console.log(`O ultimo valor do vetor é ${num[num.length-1]}`)
 num.sort()
 console.log(`O vetor em ordem fica: ${num}`)
 console.log('------------------------------------------------------')
-
+/*
 for (let c = 0; c <= 4; c++) {
     console.log(num[c])
+}
+*/
+for (let pos in num) {
+    console.log(`A posição ${pos} tem o valor ${num[pos]}`)
 }
