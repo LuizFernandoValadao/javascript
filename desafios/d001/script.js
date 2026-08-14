0b4c9ea57f71e607dde738bd9682dba452860739
LuizFernandoValadao <l17996021823@gmail.com>
2026-08-13 22:49:50 -0300

    d001

--- desafios/d001/script.js
+++ desafios/d001/script.js
@@ -1,3 +1,6 @@
+let list = []
+let cont = 1
+
 function adicionar() {
     let txtn = document.getElementById('txtnum')
     if (txtn.value.length == 0) {
@@ -6,20 +9,30 @@ function adicionar() {
         alert('Digite um valor entre 1 e 100!')
     } else {
         let num = Number(txtn.value)
-        let list = []
-        list.push(num)
-        let select = document.getElementById('nlista')
-        let opt = document.createElement('option')
-        opt.text = num
-        opt.value = 1
-        select.appendChild(opt)
-        return list
+        let n = 0
+        while (n < list.length) {
+            if (list[n] == num) {
+                break
+            }
+            n++
+        }
+        if (list[n] == num) {
+            alert('Número já adicionado!')
+        } else {
+            list.push(num)
+            let select = document.getElementById('nlista')
+            let opt = document.createElement('option')
+            opt.text = `Valor ${num} adicionado`
+            opt.value = cont
+            select.appendChild(opt)
+            cont++
+        }
+        
 
     }
 }
 
-let l = adicionar()
-
 function finalizar() {
-    alert(`45454${l}`)
+    let res = document.getElementById('res')
+    res.innerText = `Ao todo, temos ${list.length} números cadastrados.`
 }
